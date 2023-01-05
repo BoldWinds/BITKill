1fbb38fcf31e70d9eeb7875f15ac3ff13f8d524d
Rosy <lc_rosy@qq.com>
2023-01-05 16:12:56 +0800

    UserDao：删除了一些弃用的函数 UserDaoImpl：增加错误处理 LoginService/RegisterService：编写完成 GameHandler：增加了注册和登录service的配套代码 现在注册和登录功能可正常使用，已测试

--- src/main/java/cn/edu/bit/BITKill/handler/GameHandler.java
+++ src/main/java/cn/edu/bit/BITKill/handler/GameHandler.java
@@ -2,15 +2,14 @@ package cn.edu.bit.BITKill.handler;
 
 
 import cn.edu.bit.BITKill.model.CommonParam;
+import cn.edu.bit.BITKill.model.CommonResp;
+import cn.edu.bit.BITKill.model.GlobalData;
 import cn.edu.bit.BITKill.model.UserParam;
 import cn.edu.bit.BITKill.service.LoginService;
 import cn.edu.bit.BITKill.service.RegisterService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.*;
 
 public class GameHandler implements WebSocketHandler {
 
@@ -44,21 +43,61 @@ public class GameHandler implements WebSocketHandler {
                 UserParam userParam = registerCommonParam.getContent();
                 if(registerService.register(userParam)){
                     //注册成功
-
+                    CommonResp<Object> regSucResp = new CommonResp<>("register", true, "Registration successful", null);
+                    String regSucRespStr = objectMapper.writeValueAsString(regSucResp);
+                    //System.out.println(regSucRespStr);
+                    session.sendMessage(new TextMessage(regSucRespStr));
                 }else{
                     //注册失败
-
+                    CommonResp<Object> regFailResp = new CommonResp<>("register", false, "Duplicate username", null);
+                    String regFailRespStr = objectMapper.writeValueAsString(regFailResp);
+                    //System.out.println(regFailRespStr);
+                    session.sendMessage(new TextMessage(regFailRespStr));
                 }
                 break;
             case "login":
                 CommonParam<UserParam> loginCommonParam = objectMapper.readValue(paramJson, new TypeReference<CommonParam<UserParam>>(){});
                 UserParam userParam1 = loginCommonParam.getContent();
+                CommonResp<Object> loginResp = new CommonResp<>("login", true, "Login successful", null);
+                switch (loginService.login(userParam1))
+                {
+                    //登录成功
+                    case 0:
+                        GlobalData.userLogin(userParam1.getUsername(),session);
+                        break;
+                    //用户名错误
+                    case 1:
+                        loginResp.setSuccess(false);
+                        loginResp.setMessage("Wrong username");
+                        break;
+                    //密码错误
+                    case 2:
+                        loginResp.setSuccess(false);
+                        loginResp.setMessage("Wrong password");
+                        break;
+                    //重复登录
+                    case 3:
+                        loginResp.setSuccess(false);
+                        loginResp.setMessage("Already login");
+                        break;
+                }
+                String loginRespStr = objectMapper.writeValueAsString(loginResp);
+                session.sendMessage(new TextMessage(loginRespStr));
+                /*
                 if(loginService.login(userParam1)){
                     // 登录成功
+                    GlobalData.userLogin(userParam1.getUsername(),session);
 
+                    CommonResp<Object> loginSucResp = new CommonResp<>("login", true, "Login successful", null);
+                    String loginSucRespStr = objectMapper.writeValueAsString(loginSucResp);
+                    session.sendMessage(new TextMessage(loginSucRespStr));
                 }else{
                     // 登录失败
+                    CommonResp<Object> loginFailResp = new CommonResp<>("login", true, "Login successful", null);
+                    String loginFailRespStr = objectMapper.writeValueAsString(loginFailResp);
+                    session.sendMessage(new TextMessage(loginFailRespStr));
                 }
+                 */
                 break;
             case "create room":
                 break;
--- src/main/java/cn/edu/bit/BITKill/service/LoginService.java
+++ src/main/java/cn/edu/bit/BITKill/service/LoginService.java
@@ -1,6 +1,7 @@
 package cn.edu.bit.BITKill.service;
 
 import cn.edu.bit.BITKill.dao.UserDaoImpl;
+import cn.edu.bit.BITKill.model.GlobalData;
 import cn.edu.bit.BITKill.model.UserParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,24 @@ public class LoginService {
         this.userDaoImpl = userDaoImpl;
     }
 
-    public boolean login(UserParam userParam){
-        return false;
+    public int login(UserParam userParam){
+        UserParam dbUser = userDaoImpl.getUser(userParam.getUsername());
+        if(dbUser == null)
+        {
+            //用户名错误
+            return 1;
+        }
+        if(!dbUser.getPassword().equals(userParam.getPassword()))
+        {
+            //密码错误
+            return 2;
+        }
+        if(GlobalData.getSessionByUsername(userParam.getUsername()) != null)
+        {
+            //重复登录，视为登录失败
+            return 3;
+        }
+        //登录成功
+        return 0;
     }
 }
